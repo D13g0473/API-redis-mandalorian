3e5b6327245ee2af90da76985323f2e2c7fe139d
Diego Miguel Arebalo <diego.arebalo@liotgroup.io>
2025-04-08 13:52:17 -0300

    web of Mandalorian

---
+++ mando-app/src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class=" background">
+    <q-header elevated  class="bg-dark">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Mando Store
+        </q-toolbar-title>
+
+        <div>lucal's films</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.background{
+  background-image: url("../assets/3rs2yx311cu31.webp");
+  background-size:cover ;
+}
+</style>
---
+++ mando-app/src/pages/IndexPage.vue
@@ -0,0 +1,145 @@
+<template>
+  <q-page class="q-pa-md" >
+
+      <q-table
+        :rows="chapters"
+        :columns="columns"
+        row-key="id"
+        flat
+        bordered
+        class="my-table"
+        elevated
+      >
+      <template v-slot:top>
+        <div class="row col-12">
+          <div class="col-11">
+            <center>
+              <h5>{{ title }}</h5>
+            </center>    
+          </div>
+          <q-space></q-space>
+          <div class="col-1">
+            <q-btn
+                icon="refresh"
+                @click="fetchChapters()"
+                />
+          </div>   
+        </div>
+  
+      </template>
+        <template v-slot:body-cell-status="props" >
+          <q-td  align="center" class="q-gutter-xs" style="width: 120px;">
+            <q-chip
+             square
+            :color="getStatusColor(props.row.status)"
+            class="text-white"
+          >
+            {{ props.row.status }}
+          </q-chip>
+          </q-td>
+
+        </template>
+        
+
+        <template v-slot:body-cell-actions="props">
+          <q-td  align="center" class="q-gutter-xs" style="width: 120px;" >
+            <q-btn
+            v-if="props.row.status === 'available'"
+            color="primary"
+            @click="reserveChapter(props.row.id)"
+          >
+            Reservar
+          </q-btn>
+          <q-btn
+            v-if="props.row.status === 'reserved'"
+            color="green"
+            @click="confirmChapter(props.row.id)"
+          >
+            Confirmar
+          </q-btn>
+          </q-td>
+        </template>
+      </q-table>
+    <!-- </q-card> -->
+  </q-page>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title:"Capítulos de The Mandalorian",
+      chapters: [], // Datos de los capítulos
+      columns: [
+      { name: "id", label: "ID", align: "left", field: "id" },
+      { name: "title", label: "Título", align: "left", field: "title" },
+      { name: "status", label: "Estado", align: "center", field: "status" },
+      { name: "actions", label: "Acciones", align: "left", field: "actions" },
+      ],
+    };
+  },
+  methods: {
+    async fetchChapters() {
+      try {
+        const response = await axios.get("http://localhost:5000/chapters");
+        console.log(response);
+        this.chapters = response.data;
+      } catch (error) {
+        console.error("Error fetching chapters:", error);
+      }
+    },
+    async reserveChapter(chapterId) {
+      try {
+        const response = await axios.post(
+          `http://localhost:5000/rent/${chapterId}`
+        );
+        alert(response.data.message);
+        this.$q.notify({ type: "positive", message: response.data.message });
+        this.fetchChapters(); // Actualiza la lista de capítulos
+      } catch (error) {
+        this.$q.notify({ type: "negative", message: error.response.data.error });
+      }
+    },
+    async confirmChapter(chapterId) {
+      try {
+        const response = await axios.post(
+          `http://localhost:5000/confirm/${chapterId}`,
+          { price: 4.99 }
+        );
+        this.$q.notify({ type: "positive", message: response.data.message });
+        this.fetchChapters(); // Actualiza la lista de capítulos
+      } catch (error) {
+        this.$q.notify({ type: "negative", message: error.response.data.error });
+      }
+    },
+    getStatusColor(status) {
+      switch (status) {
+        case "available":
+          return "green";
+        case "reserved":
+          return "orange";
+        case "rented":
+          return "red";
+        default:
+          return "grey";
+      }
+    },
+  },
+  mounted() {
+    this.fetchChapters();
+  },
+};
+</script>
+
+<style>
+.my-table {
+  max-width: 800px;
+  margin: auto;
+  background-color: rgba(116, 132, 143, 0.918);
+}
+.transparent{
+  background-color: rgba(58, 57, 57, 0);
+}
+</style>
